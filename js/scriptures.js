42b5df16d78d0960d7b4e463f59f50b8ccb86b20
Nathan Sonnenberg <65731867+ntsonnenberg@users.noreply.github.com>
2022-03-02 22:00:08 -0700

    tried configuring animations between chapters and still failed

--- js/scriptures.js
+++ js/scriptures.js
@@ -352,6 +352,13 @@ const Scriptures = (function () {
 			element.innerHTML += `<div class="nextprev">${requestedNextPrevious}</div>`;
 		});
 
+		let scripWrapper = document.getElementsByClassName("scripturewrapper")[0];
+		console.log(scripWrapper);
+
+		scripWrapper.addEventListener("animationstart");
+		scripWrapper.addEventListener("animationend");
+		scripWrapper.addEventListener("animationiteration");
+
 		injectBreadcrumbs(volumeForId(book.parentBookId), book, requestedChapter);
 		setupMarkers();
 	};
